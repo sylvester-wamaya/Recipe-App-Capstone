a129960a9197f4319fb7c638187fdeccd1558ed2
haftamudesta <desta451616@hotmail.com>
2023-09-13 16:41:30 +0300

    Add cancan ability model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,14 +1,2 @@
 class ApplicationRecord < ActiveRecord::Base
-  before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
-
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:warning] = exception.message
-    redirect_to recipes_path
-  end
-
-  protected
-
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:name, :email, :password) }
-  end
 end
