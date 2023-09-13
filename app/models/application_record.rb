fcc43fea695060484423804eab3930b9c736e4d1
haftamudesta <desta451616@hotmail.com>
2023-09-13 15:11:07 +0300

    Add authentication using devise gem

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:warning] = exception.message
+    redirect_to recipes_path
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:name, :email, :password) }
+  end
 end
