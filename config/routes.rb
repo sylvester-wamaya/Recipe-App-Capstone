e069c9fa9d88090c045cc90d6143699ec5762458
haftamudesta <desta451616@hotmail.com>
2023-09-14 21:14:08 +0300

    Modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :foods
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "recipes#index"
+   root "recipes#create"
    resources :recipes
+   resources :foods
 end
