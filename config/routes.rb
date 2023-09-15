5d5833b8beee7abd3566683bedad6b52119b76c1
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-15 14:13:14 +0300

    Add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "recipes#create"
+   root "pages#home"
   resources :users, only: [:index, :show] do
     resources :recipes
     resources :foods, only: [:index, :show, :new, :create, :destroy]
