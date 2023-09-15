d61395033dab847ca83ab52fbbd7ff4a47e89e98
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-15 16:13:24 +0300

    Update default page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "pages#home"
+   root "users#index"
   resources :users, only: [:index, :show] do
     resources :recipes
     resources :foods, only: [:index, :show, :new, :create, :destroy]
