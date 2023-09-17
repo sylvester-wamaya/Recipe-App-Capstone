d5110d3e46497f83dfcbb27ad00a754837bdbff1
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-17 05:34:48 +0300

    Add styling

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -37,25 +37,22 @@ class RecipeFoodsController < ApplicationController
 
     respond_to do |format|
       if @recipe_food.save
-        format.html { redirect_to recipe_url(@recipe_food.recipe), notice: 'Recipe food was successfully created.' }
-        format.json { render :show, status: :created, location: @recipe_food }
+        format.html { redirect_to recipes_path(@recipe), notice: 'Recipe food was successfully created.' }
+       
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
+       
       end
     end
   end
 
   # PATCH/PUT /recipe_foods/1 or /recipe_foods/1.json
   def update
-    respond_to do |format|
-      if @recipe_food.update(recipe_food_params)
-        format.html { redirect_to recipe_url(@recipe_food.recipe), notice: 'Recipe food was successfully updated.' }
-        format.json { render :show, status: :ok, location: @recipe_food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
-      end
+    @recipe_food = RecipeFood.find(params[:id])
+    if @recipe_food.update(recipe_food_params)
+      redirect_to recipe_path(@recipe_food.recipe), notice: 'Recipe Food was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
