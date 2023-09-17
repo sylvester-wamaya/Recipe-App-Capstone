5ba715fa7ef1857e378436dd659b3b68adcd241e
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-17 05:35:29 +0300

    Update the food views

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,8 @@
 class Food < ApplicationRecord
-  has_many :recipe_foods
+
+  has_many :recipe_foods, dependent: :destroy
   belongs_to :user
 
-  validates :name, :measurement_unit, :price, :quantity, presence: true
+  validates :name, :measurement_unit,  presence: true
+  validates :price, :quantity, presence: true, numericality: {greater_than: 0}
 end
